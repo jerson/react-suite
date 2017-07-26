7b4946f73abf315a139c12d5ba89cf0cd6d3d035
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2017-07-25 23:14:26 -0500

    added command watch to package.json

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,147 +1,155 @@
 import * as React from 'react';
-import {StyleSheet, TextInput, TextInputProperties, TextStyle, ViewStyle} from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  TextInputProperties,
+  TextStyle,
+  ViewStyle
+} from 'react-native';
 import Text from './Text';
 import View from './View';
 import Theme from '../../modules/theme/Theme';
 
 export interface InputProps extends TextInputProperties {
-    value?: string;
-    placeholder?: string;
-    defaultValue?: string;
-    hasError?: boolean;
-    style?: ViewStyle;
-    labelStyle?: TextStyle;
-    containerStyle?: ViewStyle;
-    onChangeText?: (text: string) => void;
-    useLabel?: boolean;
+  value?: string;
+  placeholder?: string;
+  defaultValue?: string;
+  hasError?: boolean;
+  style?: ViewStyle;
+  labelStyle?: TextStyle;
+  containerStyle?: ViewStyle;
+  onChangeText?: (text: string) => void;
+  useLabel?: boolean;
 }
 
 export interface State {
-    value: string;
+  value: string;
 }
 
 export default class Input extends React.Component<InputProps, State> {
-    state = {
-        value: ''
-    };
+  state = {
+    value: ''
+  };
 
-    refs: {
-        [string: string]: any;
-        input: TextInput;
-    };
+  refs: {
+    [string: string]: any;
+    input: TextInput;
+  };
 
-    setValue(value: string) {
-        this.setState({value});
+  setValue(value: string) {
+    this.setState({ value });
+  }
+
+  getValue() {
+    return this.state.value;
+  }
+
+  focus() {
+    this.refs.input.focus();
+  }
+
+  onChangeText(text: string) {
+    this.setValue(text);
+    if (typeof this.props.onChangeText === 'function') {
+      this.props.onChangeText(text);
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.defaultValue) {
+      this.setValue(this.props.defaultValue);
+    }
+  }
+
+  render() {
+    let {
+      value,
+      defaultValue,
+      hasError,
+      labelStyle,
+      containerStyle,
+      style,
+      useLabel,
+      placeholder,
+      ...props
+    } = this.props;
+
+    let newProps: TextInputProperties = { ...props };
+    newProps.value = this.state.value;
+    newProps.onChangeText = this.onChangeText.bind(this);
+    if (!useLabel) {
+      newProps.placeholder = placeholder;
     }
 
-    getValue() {
-        return this.state.value;
-    }
+    return (
+      <View style={[styles.container]}>
+        {useLabel &&
+          <Text
+            style={[styles.label, hasError && styles.labelError, labelStyle]}
+          >
+            {placeholder}
+          </Text>}
+        <View
+          style={[
+            styles.inputContainer,
+            hasError && styles.inputContainerError,
+            containerStyle
+          ]}
+        >
+          <TextInput
+            underlineColorAndroid='transparent'
+            placeholderTextColor={
+              hasError
+                ? Theme.vars.inputErrorPlaceholderColor
+                : Theme.vars.inputPlaceholderColor
+            }
+            style={[styles.input, hasError && styles.inputError, style]}
+            ref='input'
+            {...newProps}
+          />
+        </View>
 
-    focus() {
-        this.refs.input.focus();
-    }
-
-    onChangeText(text: string) {
-        this.setValue(text);
-        if (typeof this.props.onChangeText === 'function') {
-            this.props.onChangeText(text);
-        }
-    }
-
-    componentDidMount() {
-        if (this.props.defaultValue) {
-            this.setValue(this.props.defaultValue);
-        }
-    }
-
-    render() {
-        let {
-            value,
-            defaultValue,
-            hasError,
-            labelStyle,
-            containerStyle,
-            style,
-            useLabel,
-            placeholder,
-            ...props
-        } = this.props;
-
-        let newProps: TextInputProperties = {...props};
-        newProps.value = this.state.value;
-        newProps.onChangeText = this.onChangeText.bind(this);
-        if (!useLabel) {
-            newProps.placeholder = placeholder;
-        }
-
-        return (
-            <View style={[styles.container]}>
-                {useLabel &&
-                <Text style={[styles.label, hasError && styles.labelError, labelStyle]}>
-                    {placeholder}
-                </Text>}
-                <View
-                    style={[
-                        styles.inputContainer,
-                        hasError && styles.inputContainerError,
-                        containerStyle
-                    ]}
-                >
-                    <TextInput
-                        underlineColorAndroid='transparent'
-                        placeholderTextColor={
-                            hasError
-                                ? Theme.vars.inputErrorPlaceholderColor
-                                : Theme.vars.inputPlaceholderColor
-                        }
-                        style={[styles.input, hasError && styles.inputError, style]}
-                        ref='input'
-                        {...newProps}
-                    />
-                </View>
-
-            </View>
-        );
-    }
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        paddingTop: 4,
-        paddingBottom: 10
-    } as ViewStyle,
-    label: {
-        color: Theme.vars.inputLabelColor,
-        textAlign: 'center',
-        padding: 4
-    } as TextStyle,
-    labelError: {
-        color: Theme.vars.inputErrorLabelColor
-    } as TextStyle,
-    inputContainer: {
-        //padding: 1,
-        backgroundColor: Theme.vars.inputBackgroundColor,
-        borderColor: Theme.vars.inputBorderColor,
-        borderWidth: 1,
-        borderRadius: 4
-    } as ViewStyle,
-    inputContainerError: {
-        backgroundColor: Theme.vars.inputErrorBackgroundColor,
-        borderColor: Theme.vars.inputErrorBorderColor
-    } as ViewStyle,
-    input: {
-        height: 35,
-        backgroundColor: Theme.vars.inputBackgroundColor,
-        color: Theme.vars.inputTextColor,
-        borderRadius: 4,
-        fontSize: 13,
-        padding: 4,
-        paddingLeft: 10,
-        paddingRight: 10
-    } as TextStyle,
-    inputError: {
-        backgroundColor: Theme.vars.inputErrorBackgroundColor,
-        color: Theme.vars.inputErrorTextColor
-    } as ViewStyle
+  container: {
+    paddingTop: 4,
+    paddingBottom: 10
+  } as ViewStyle,
+  label: {
+    color: Theme.vars.inputLabelColor,
+    textAlign: 'center',
+    padding: 4
+  } as TextStyle,
+  labelError: {
+    color: Theme.vars.inputErrorLabelColor
+  } as TextStyle,
+  inputContainer: {
+    //padding: 1,
+    backgroundColor: Theme.vars.inputBackgroundColor,
+    borderColor: Theme.vars.inputBorderColor,
+    borderWidth: 1,
+    borderRadius: 4
+  } as ViewStyle,
+  inputContainerError: {
+    backgroundColor: Theme.vars.inputErrorBackgroundColor,
+    borderColor: Theme.vars.inputErrorBorderColor
+  } as ViewStyle,
+  input: {
+    height: 35,
+    backgroundColor: Theme.vars.inputBackgroundColor,
+    color: Theme.vars.inputTextColor,
+    borderRadius: 4,
+    fontSize: 13,
+    padding: 4,
+    paddingLeft: 10,
+    paddingRight: 10
+  } as TextStyle,
+  inputError: {
+    backgroundColor: Theme.vars.inputErrorBackgroundColor,
+    color: Theme.vars.inputErrorTextColor
+  } as ViewStyle
 });
--- src/components/ui/MessageCenter.tsx
+++ src/components/ui/MessageCenter.tsx
@@ -1,124 +1,126 @@
 import * as React from 'react';
-import {Platform, StyleSheet, ViewStyle} from 'react-native';
-import MessageItem, {Message} from './MessageItem';
+import { Platform, StyleSheet, ViewStyle } from 'react-native';
+import MessageItem, { Message } from './MessageItem';
 import View from './View';
 import Log from '../../modules/logger/Log';
 
 export interface MessageCenterProps {
-    topOffset?: number;
+  topOffset?: number;
 }
 
 export interface State {
-    messages: Message[];
+  messages: Message[];
 }
 
-export default class MessageCenter extends React.Component<MessageCenterProps,
-    State> {
-    static counter = 0;
+export default class MessageCenter extends React.Component<
+  MessageCenterProps,
+  State
+> {
+  static counter = 0;
 
-    state = {
-        messages: []
-    };
+  state = {
+    messages: []
+  };
 
-    refs: {
-        [string: string]: any;
-    };
+  refs: {
+    [string: string]: any;
+  };
 
-    async show(params: Message): Promise<string> {
-        MessageCenter.counter++;
-        if (!params.id) {
-            params.id = MessageCenter.counter.toString();
-        }
-
-        let messages: Message[] = this.state.messages;
-        let exist = messages.some(message => {
-            return params.id === message.id;
-        });
-
-        if (!exist) {
-            messages = [...messages, params];
-        } else {
-            messages = messages.map(message => {
-                if (params.id === message.id) {
-                    return params;
-                }
-                return message;
-            });
-        }
-
-        Log.info(messages.length, MessageCenter.counter);
-
-        return new Promise<string>((resolve, reject) => {
-            this.setState({messages}, () => {
-                resolve(params.id);
-            });
-        });
+  async show(params: Message): Promise<string> {
+    MessageCenter.counter++;
+    if (!params.id) {
+      params.id = MessageCenter.counter.toString();
     }
 
-    remove(id: string) {
-        let messages = this.state.messages.filter((item: Message) => {
-            return item.id !== id;
-        });
-        this.setState({messages});
-    }
+    let messages: Message[] = this.state.messages;
+    let exist = messages.some(message => {
+      return params.id === message.id;
+    });
 
-    onHideItem(id: string) {
-        let messageRef: MessageItem = this.refs[`message${id}`];
-        if (messageRef) {
-            messageRef.hide(() => {
-                this.remove(id);
-            });
+    if (!exist) {
+      messages = [...messages, params];
+    } else {
+      messages = messages.map(message => {
+        if (params.id === message.id) {
+          return params;
         }
+        return message;
+      });
     }
 
-    render() {
-        let {messages} = this.state;
-        let {topOffset} = this.props;
-        let top = topOffset || 5;
+    Log.info(messages.length, MessageCenter.counter);
 
-        if (Platform.OS === 'ios') {
-            //status bar
-            top += 20;
-        }
+    return new Promise<string>((resolve, reject) => {
+      this.setState({ messages }, () => {
+        resolve(params.id);
+      });
+    });
+  }
 
-        return (
-            <View style={[styles.container, {top}]}>
-                <View
-                    style={[styles.content, Platform.OS === 'web' && styles.contentWeb]}
-                >
-                    {messages.map((item: Message, index) => {
-                        return (
-                            <MessageItem
-                                key={item.id}
-                                ref={`message${item.id}`}
-                                onHide={this.onHideItem.bind(this, item.id)}
-                                item={item}
-                            />
-                        );
-                    })}
-                </View>
-            </View>
-        );
+  remove(id: string) {
+    let messages = this.state.messages.filter((item: Message) => {
+      return item.id !== id;
+    });
+    this.setState({ messages });
+  }
+
+  onHideItem(id: string) {
+    let messageRef: MessageItem = this.refs[`message${id}`];
+    if (messageRef) {
+      messageRef.hide(() => {
+        this.remove(id);
+      });
     }
+  }
+
+  render() {
+    let { messages } = this.state;
+    let { topOffset } = this.props;
+    let top = topOffset || 5;
+
+    if (Platform.OS === 'ios') {
+      //status bar
+      top += 20;
+    }
+
+    return (
+      <View style={[styles.container, { top }]}>
+        <View
+          style={[styles.content, Platform.OS === 'web' && styles.contentWeb]}
+        >
+          {messages.map((item: Message, index) => {
+            return (
+              <MessageItem
+                key={item.id}
+                ref={`message${item.id}`}
+                onHide={this.onHideItem.bind(this, item.id)}
+                item={item}
+              />
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    content: {
-        zIndex: 100,
-        minWidth: 200
-    } as ViewStyle,
-    contentWeb: {
-        maxWidth: 500,
-        alignSelf: 'center'
-    } as ViewStyle,
-    container: {
-        overflow: 'hidden',
-        zIndex: 100,
-        elevation: 10,
-        flex: 1,
-        //position: Platform.OS === 'web' ? 'fixed' : 'absolute',
-        position: 'absolute',
-        right: 5,
-        left: 5
-    } as ViewStyle
+  content: {
+    zIndex: 100,
+    minWidth: 200
+  } as ViewStyle,
+  contentWeb: {
+    maxWidth: 500,
+    alignSelf: 'center'
+  } as ViewStyle,
+  container: {
+    overflow: 'hidden',
+    zIndex: 100,
+    elevation: 10,
+    flex: 1,
+    //position: Platform.OS === 'web' ? 'fixed' : 'absolute',
+    position: 'absolute',
+    right: 5,
+    left: 5
+  } as ViewStyle
 });
